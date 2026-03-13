3cf69fa034ed8ea91b8171d52eb2a502df6fbbed
rakdav <rakdav@inbox.ru>
2026-03-13 17:11:47 +0200

    last

--- BinaryTree/BinaryTreeNode.cs
+++ BinaryTree/BinaryTreeNode.cs
@@ -9,6 +9,7 @@ namespace BinaryTree
     public class BinaryTreeNode<T> : TreeNode<T>
     {
         public BinaryTreeNode() => Children = new List<TreeNode<T>>() { null, null };
+        public BinaryTreeNode<T> Parent { get; set; }
         public BinaryTreeNode<T> Left
         {
             get { return (BinaryTreeNode<T>)Children[0]; }
@@ -17,6 +18,17 @@ namespace BinaryTree
         public BinaryTreeNode<T> Right {
             get { return (BinaryTreeNode<T>)Children[1]; }
             set { Children[1] = value; }
+        }
+        public int GetHeight()
+        {
+            int height = 1;
+            BinaryTreeNode<T> current = this;
+            while (current.Parent != null)
+            {
+                height++;
+                current = current.Parent;
+            }
+            return height;
         }
     }
 }
